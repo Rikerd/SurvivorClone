fac7fd8778c2d79d111e4d197df0996f6324c89d
Richard But <cursedmarkedsasuke@gmail.com>
2024-08-07 20:57:50 -0700

    Player Movement
    Create player with player movement using Unity Input System

---
+++ Survivor Clone/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    private Vector2 movement = Vector2.zero;
+
+    private Rigidbody2D rb2d;
+
+    private PlayerInput playerInput;
+    private InputAction movementAction;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        playerInput = GetComponent<PlayerInput>();
+        movementAction = playerInput.actions["Movement"];
+    }
+
+    private void OnEnable()
+    {
+        // Enable movement callbacks
+        movementAction.Enable();
+        movementAction.started += OnMovement;
+        movementAction.performed += OnMovement;
+        movementAction.canceled += OnMovement;
+    }
+
+    private void OnDisable()
+    {
+        // Disable movement callbacks
+        movementAction.Disable();
+        movementAction.started -= OnMovement;
+        movementAction.performed -= OnMovement;
+        movementAction.canceled -= OnMovement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement);
+    }
+
+    private void OnMovement(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        input.Normalize();
+        movement = input * movementSpeed * Time.deltaTime;
+    }
+}
