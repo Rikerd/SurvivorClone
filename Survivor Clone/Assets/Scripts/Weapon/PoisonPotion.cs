00e1e0227062496539d21f3cc894478a9744c84d
Richard But <cursedmarkedsasuke@gmail.com>
2024-08-20 23:21:35 -0700

    Add poison animation
    Add poison spawn animation and break sfx

--- Survivor Clone/Assets/Scripts/Weapon/PoisonPotion.cs
+++ Survivor Clone/Assets/Scripts/Weapon/PoisonPotion.cs
@@ -7,20 +7,21 @@ public class PoisonPotion : Weapon
     public SpawnedAuraStats auraStat;
 
     private float currentLifeTime = 0f;
+    private bool spawning = true;
 
     // Start is called before the first frame update
     private void Start()
     {
-        SetMaxCooldown(auraStat.levelStats[currentWeaponLevel].maxCooldown);
-
-        transform.localScale = new Vector2(auraStat.levelStats[currentWeaponLevel].radius, auraStat.levelStats[currentWeaponLevel].radius) * 2;
-
-        currentLifeTime = auraStat.levelStats[currentWeaponLevel].lifeTime;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (spawning)
+        {
+            return;
+        }
+
         bool cooldownComplete = base.UpdateCooldownTimer();
 
         currentLifeTime -= Time.deltaTime;
@@ -54,9 +55,18 @@ public class PoisonPotion : Weapon
     {
         currentWeaponLevel = level;
 
+        spawning = true;
+    }
+
+    public void FinishSpawning()
+    {
+        SetMaxCooldown(0);
         transform.localScale = new Vector2(auraStat.levelStats[currentWeaponLevel].radius, auraStat.levelStats[currentWeaponLevel].radius) * 2;
-        SetMaxCooldown(auraStat.levelStats[currentWeaponLevel].maxCooldown);
         currentLifeTime = auraStat.levelStats[currentWeaponLevel].lifeTime;
+
+        GameManager.Instance.audioSource.PlayOneShot(fireSfx);
+
+        spawning = false;
     }
 
     private void OnDrawGizmos()
