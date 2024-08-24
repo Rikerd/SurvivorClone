57a394c9b95ce0909262bfd24953c91673db47cb
Richard But <cursedmarkedsasuke@gmail.com>
2024-08-23 22:20:21 -0700

    Add UI SFX
    Add UI SFX to all buttons for on hover and select for all scenes

---
+++ Survivor Clone/Assets/Scripts/UI/AudioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    public AudioClip menuSelect;
+    public AudioClip menuMove;
+    public AudioClip menuBack;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMenuSelect()
+    {
+        audioSource.PlayOneShot(menuSelect);
+    }
+
+    public void PlayMenuMove()
+    {
+        audioSource.PlayOneShot(menuMove);
+    }
+
+    public void PlayMenuBack()
+    {
+        audioSource.PlayOneShot(menuBack);
+    }
+}
