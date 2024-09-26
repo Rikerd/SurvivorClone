8f299e5514422096cb62cb8554394c70ecb567f4
Richard But <cursedmarkedsasuke@gmail.com>
2024-09-26 16:52:26 -0700

    Update shop look
    Add level pips to indicate levels
    Add icons for each store level up

--- Survivor Clone/Assets/Scripts/StoreUpgradeButton.cs
+++ Survivor Clone/Assets/Scripts/StoreUpgradeButton.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoreUpgradeButton : MonoBehaviour, IDataPersistence
 {
     public StoreUpgradeStatCosts upgradeStat;
     public TMP_Text buttonText;
+    public GameObject levelHolder;
 
     public TMP_Text descriptionText;
 
     private int currentLevel = 0;
 
+    private Image[] levelPips;
+
+    private void Awake()
+    {
+        levelPips = levelHolder.GetComponentsInChildren<Image>();
+    }
+
     public void LoadAccountData(AccountData data)
     {
         currentLevel = data.accountUpgradeTypeLevels[(int)upgradeStat.upgradeType];
-        buttonText.SetText("Cost: " + upgradeStat.coinCosts[currentLevel] + "\n" + "Level " + currentLevel.ToString() + "\n" + upgradeStat.upgradeName);
+
+        for (int i = 0; i < currentLevel; i++)
+        {
+            levelPips[i].color = Color.red;
+        }
+        //buttonText.SetText("Cost: " + upgradeStat.coinCosts[currentLevel] + "\n" + "Level " + currentLevel.ToString() + "\n" + upgradeStat.upgradeName);
     }
 
     public void SaveAccountData(ref AccountData data)
@@ -37,15 +51,16 @@ public class StoreUpgradeButton : MonoBehaviour, IDataPersistence
         {
             CoinManager.Instance.SetTotalCoins(accountCoins - upgradeStat.coinCosts[currentLevel]);
 
+            levelPips[currentLevel].color = Color.red;
             currentLevel++;
-            if (currentLevel >= upgradeStat.coinCosts.Count)
-            {
-                buttonText.SetText("Max\n" + "Level " + currentLevel.ToString() + "\n" + upgradeStat.upgradeName);
-            }
-            else
-            {
-                buttonText.SetText("Cost: " + upgradeStat.coinCosts[currentLevel] + "\n" + "Level " + currentLevel.ToString() + "\n" + upgradeStat.upgradeName);
-            }
+            //if (currentLevel >= upgradeStat.coinCosts.Count)
+            //{
+            //    buttonText.SetText("Max\n" + "Level " + currentLevel.ToString() + "\n" + upgradeStat.upgradeName);
+            //}
+            //else
+            //{
+            //    buttonText.SetText("Cost: " + upgradeStat.coinCosts[currentLevel] + "\n" + "Level " + currentLevel.ToString() + "\n" + upgradeStat.upgradeName);
+            //}
             DataPersistenceManager.Instance.SaveAccountData();
         }
         else
@@ -56,6 +71,13 @@ public class StoreUpgradeButton : MonoBehaviour, IDataPersistence
 
     public void SetDescriptionText()
     {
-        descriptionText.SetText(upgradeStat.upgradeDescription);
+        if (currentLevel >= upgradeStat.coinCosts.Count)
+        {
+            descriptionText.SetText("Stat maxed out!\n" + upgradeStat.upgradeDescription);
+        }
+        else
+        {
+            descriptionText.SetText("Cost: " + upgradeStat.coinCosts[currentLevel] + "\n" + upgradeStat.upgradeDescription);
+        }
     }
 }
